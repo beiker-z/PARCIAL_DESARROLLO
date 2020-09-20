8b20023c08ec789ef0fb8efe6f314287bdd1de5b
beiker-z <bdchp19@gmail.com>
2020-09-19 23:04:09 -0600

    empezando proyecto_parcial

---
+++ tarea_apis/src/index.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const app= express();
+
+app.set('port',process.env.PORT || 3000);
+
+app.use(express.json());
+app.use(require('./routes/clientes'));
+app.use(require('./routes/empleados'));
+app.use(require('./routes/productos'));
+app.use(require('./routes/facturas'));
+app.use(require('./routes/productos_factura'));
+app.use(require('./routes/security'));
+
+
+app.get('/',(req,res)=>{
+    res.status(200).send('index');
+}
+);
+
+app.listen(app.get('port'),()=>{
+console.log('liaten port 3000');
+});
